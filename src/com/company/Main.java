45d5c6c82af627ab4573812247434ea6de356c91
turatbek <temirbekov.tt@gmail.com>
2022-04-09 16:25:32 +0600

    Task#3.2

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.print(a%10+ "," + a%100/10 + "," + a/100);
+
+
+
+
+
+
+
+
+
+    }
+}
